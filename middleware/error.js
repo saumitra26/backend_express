8e3bf9047b23efda3427c76fe415b371632bee81
Saumitra Bagchi <saumitra.ku.cse@gmail.com>
2026-01-03 14:50:12 +0200

    Test CI run

--- middleware/error.js
+++ middleware/error.js
@@ -1,4 +1,4 @@
-export const errorHandler = (err, req, res, next) => { 
+export const errorHandler = (err, req, res,) => { 
     const statusCode = err.status || 500
     res.status(statusCode).json({
         errorMessage: err.message
